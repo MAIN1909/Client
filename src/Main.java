7744e77de8e74f12967e02a7d7c91df5a8e66b51
Евгений <Main1909@ex.ua>
2019-01-23 00:37:52 +0200

    mission complete

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Enter message:");
+        System.out.println("Are you registered? (Yes or no)");
         String msg = new Scanner(System.in).nextLine();
-        try(Socket client = new Socket("10.11.0.173", 8899)){
+        try (Socket client = new Socket("localhost", 8899)) {
             ObjectOutputStream oos = new ObjectOutputStream(
                     client.getOutputStream());
-            oos.writeObject(msg);
+            switch (msg) {
+                case "yes":
+                    System.out.println("Enter your name:");
+                    String name = new Scanner(System.in).nextLine();
+                    oos.writeObject(name);
+                    System.out.println("Enter your message");
+                    String message = new Scanner(System.in).nextLine();
+                    oos.writeObject(message);
+                    break;
+                case "no":
+                    System.out.println("Enter 'registration'");
+                    String registration = new Scanner(System.in).nextLine();
+                    oos.writeObject(registration);
+                    System.out.println("Enter your name:");
+                    String namE = new Scanner(System.in).nextLine();
+                    oos.writeObject(namE);
+                    System.out.println("Enter your message");
+                    String messagE = new Scanner(System.in).nextLine();
+                    oos.writeObject(messagE);
+
+            }
             oos.close();
         }catch (Exception e){
             e.printStackTrace();
